53c52206e29ec93665830af1502a3ea4e72bec9a
Roberto Alsina <ralsina@netmanagers.com.ar>
2010-04-06 11:19:26 -0300

    El principio del cap?tulo de testing

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@ DOC=libro.pdf
 
 FIGURAS=dependencias.graph.pdf loop-n-y-medio.graph.pdf
 CAPITULOS=1.txt 2.txt 3.txt 4.txt 5.txt 6.txt 7.txt 8.txt 9.txt 10.txt 11.txt 12.txt licencia.txt
+LISTADOS=gaso1.py gaso2.py
 
 %.graph.pdf: %.dot
 	dot -Tpdf $< > $@
 
-libro.pdf: cover.tmpl indice.txt ${CAPITULOS} ${FIGURAS} Makefile estilo.style
+libro.pdf: cover.tmpl indice.txt ${CAPITULOS} ${FIGURAS} Makefile estilo.style ${LISTADOS}
 	rst2pdf -e inkscape -l es_ES -b1 --smart-quotes=1 -s eightpoint,bw,estilo indice.txt -o libro.pdf --custom-cover=cover.tmpl
 
 commit:
---
+++ gaso1.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import re
+import unicodedata
+
+def gas(letra):
+    '''Dada una letra X devuelve XgasX excepto si X es una vocal acentuada,
+    en cuyo caso devuelve la primera X sin acento.
+
+    El uso de normalize lo saqué de google.
+    '''
+    return u'%sgas%s'%(unicodedata.normalize('NFKD', letra).\
+    encode('ASCII', 'ignore'), letra)
+
+
+def gasear(palabra):
+    '''Dada una palabra, la convierte al rosarino'''
+
+    # El caso obvio: acentos.
+    # Lo resolvemos con una regexp
+
+    # Uso \xe1 etc, porque así se puede copiar y pegar en un
+    # archivo sin importar el encoding.
+
+    if re.search(u'[\xe1\xe9\xed\xf3\xfa]',palabra):
+        return re.sub(u'([\xe1\xe9\xed\xf3\xfa])',
+            lambda x: gas(x.group(0)),palabra,1)
+    return palabra
+
