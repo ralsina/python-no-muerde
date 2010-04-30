7f460f5c3fb9f604945ab7ed3d2fa1f9931af9ed
Roberto Alsina <ralsina@netmanagers.com.ar>
2010-04-30 12:08:39 -0300

    muestra la ventana

--- codigo/5/radio1.py
+++ codigo/5/radio1.py
@@ -5,14 +5,24 @@
 import os,sys
 
 # Importamos los módulos de Qt
-from PyQt4 import QtCore,QtGui
+from PyQt4 import QtCore, QtGui, uic
+
+class Main(QtGui.QDialog):
+    """La ventana principal de la aplicación."""
+    def __init__(self):
+        QtGui.QDialog.__init__(self)
+        
+        # Cargamos la interfaz desde el archivo .ui
+        uifile = os.path.join(
+            os.path.abspath(
+                os.path.dirname(__file__)),'radio.ui')
+        self.ui = uic.loadUi(uifile, self)
 
 def main():
     app = QtGui.QApplication(sys.argv)
     window=Main()
     window.show()
     sys.exit(app.exec_())
-    
 
 if __name__ == "__main__":
     main()
