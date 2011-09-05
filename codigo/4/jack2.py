feb2cb816116def56621d3c5d94c986c2a5724b1
Roberto Alsina <ralsina@netmanagers.com.ar>
2011-09-05 11:04:34 -0300

    Usando un poco mas el ejemplo nuevo. (cap 4)

--- codigo/4/jack2.py
+++ codigo/4/jack2.py
@@ -8,8 +8,30 @@ def selecciona_lineas(lineas, desde=0, hasta=-1):
 
     >>> list(selecciona_lineas(range(10), 5, 10))
     [5, 6, 7, 8, 9]
+    >>> list(selecciona_lineas(range(10), -5, 1))
+    [0]
+    >>> list(selecciona_lineas(range(10), 5, 100))
+    [5, 6, 7, 8, 9]
+    >>> list(selecciona_lineas(range(10), 5, -1))
+    []
     """
 
     for i, l in enumerate(lineas):
         if desde <= i < hasta:
             yield(l)
+
+
+def selecciona_columnas(lineas, desde=0, hasta=-1):
+    """Filtra el texto dejando sólo las columnas [desde:hasta].
+
+    Soporta índices positivos y negativos con la misma semántica
+    de los slices de python.
+
+    >>> list(selecciona_columnas(("ornitorrinco",) * 5, 5, 10))
+    ['orrin', 'orrin', 'orrin', 'orrin', 'orrin']
+    >>> list(selecciona_columnas(("ornitorrinco",) * 5, 5, 99999))
+    ['orrinco', 'orrinco', 'orrinco', 'orrinco', 'orrinco']
+    """
+
+    for l in lineas:
+        yield(l[desde:hasta])
